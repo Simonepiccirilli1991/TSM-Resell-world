00c5422f5e10218173f2ccac8895fce432aa5a9b
SimonePiccirilli <simone.piccirilli91@gmail.com>
2025-03-04 23:35:37 +0100

    added reactive applicatin

--- db/src/main/java/com/tsm/resell/world/db/service/acquisti/UpdateAcquistiService.java
+++ db/src/main/java/com/tsm/resell/world/db/service/acquisti/UpdateAcquistiService.java
@@ -1,4 +1,14 @@
 package com.tsm.resell.world.db.service.acquisti;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class UpdateAcquistiService {
+
+
+    //TODO: questa e un po piu complessa, pensiamoci su come gestirla, poi passare a vendite
 }
