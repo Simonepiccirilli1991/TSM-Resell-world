faba83c2c1a32400519f3ea1ca848991a87193fe
SimonePiccirilli <simone.piccirilli91@gmail.com>
2025-03-11 23:44:22 +0100

    started vendite

---
+++ db/src/main/java/com/tsm/resell/world/db/controller/VenditeCarteController.java
@@ -0,0 +1,11 @@
+package com.tsm.resell.world.db.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/carte/vendite")
+public class VenditeCarteController {
+
+    //TODO: aggiungere parte vebdutem acquisto completata
+}
